4be0e28c138e2550737d55069f9f3cbe72f1a812
bndao <bndao@student.42.fr>
2016-07-21 21:08:11 +0200

    MONGODATABASEBACKUP

--- createAndRunContainer.sh
+++ createAndRunContainer.sh
@@ -1,5 +1,14 @@
-mv data ../MONGODATABASEBACKUP/
-docker-compose up --build
-mv ../MONGODATABASEBACKUP/data .
-sudo chmod 777 -R data
-sh mountVol.sh
+if [ -d "./data" ];then
+	mv data MONGODATABASEBACKUP/
+	docker-compose up --build
+	mv MONGODATABASEBACKUP/data .
+	sudo chmod 777 -R data
+	sh mountVol.sh
+else
+	mkdir MONGODATABASEBACKUP/data
+	docker-compose up --build
+	mv MONGODATABASEBACKUP/data .
+	sudo chmod 777 -R data
+	sh mountVol.sh
+fi
+
